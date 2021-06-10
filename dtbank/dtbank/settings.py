9840ad9e1e547d48e311509b410ed389f4c273f3
Mehmet Selahaddin Şentop <56365256+mssentop@users.noreply.github.com>
2021-06-10 03:23:08 +0300

    updated

--- dtbank/dtbank/settings.py
+++ dtbank/dtbank/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'dtbank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dtbank',
+        'USER': 'root',
+        'PASSWORD': 'localdb',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
